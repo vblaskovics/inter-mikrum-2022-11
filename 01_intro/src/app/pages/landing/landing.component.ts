ceca981dd0b24151ffae27bc3237b6edd2ea66af
vblaskovics <viktor.blaskovics@gmail.com>
2022-11-02 16:39:35 +0100

    Add combineLatest to merge streams

--- 01_intro/src/app/pages/landing/landing.component.ts
+++ 01_intro/src/app/pages/landing/landing.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import {
+  Observable,
+  map,
+  Subject,
+  switchMap,
+  tap,
+  BehaviorSubject,
+  combineLatest,
+} from 'rxjs';
 import { Post } from 'src/app/posts/post';
 import { PostService } from 'src/app/posts/post.service';
 import { Todo } from 'src/app/todos/todo';
@@ -15,25 +23,57 @@ import { UserService } from 'src/app/users/user.service';
 export class LandingComponent implements OnInit {
   users$: Observable<User[]>;
   todosById$: Observable<Todo[]> = new Observable();
-  postsByUserId$: Observable<Post[]> = new Observable();
-  completedFilter: boolean = false;
+
+  selectedUserId$: Subject<number> = new Subject<number>();
+  userIdModify$: Observable<string> = new Observable();
+  completedFilter$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+  filteredTodos$: Observable<Todo[]> = new Observable();
 
   constructor(
     private userService: UserService,
-    private todoService: TodoService,
-    private postService: PostService
+    private todoService: TodoService
   ) {
     this.users$ = this.userService.getUsers();
   }
 
-  ngOnInit(): void {}
+  get completedFilter(): boolean {
+    return this.completedFilter$.getValue();
+  }
+
+  set completedFilter(value: boolean) {
+    this.completedFilter$.next(value);
+  }
+
+  ngOnInit(): void {
+    // this.userIdModify$ = this.selectedUserId$.pipe(
+    //   map((userId) => 'hello' + userId * 2)
+    // );
+
+    this.todosById$ = this.selectedUserId$.pipe(
+      // tap((userId) => console.log('userId', userId)),
+      switchMap((userId) => this.todoService.getTodosByUserId(userId))
+    );
+
+    // combineLatest([this.todosById$, this.completedFilter$]).subscribe(
+    //   ([todos, completedFilter]) => {
+    //     console.log('todos', todos);
+    //     console.log('completedFilter', completedFilter);
+    //   }
+    // );
+
+    this.filteredTodos$ = combineLatest([this.todosById$, this.completedFilter$]).pipe(
+      map(([todos, completedFilter]) => {
+        return todos.filter((todo) => {
+          return completedFilter ? todo.completed : true;
+        });
+      })
+    );
+  }
 
   onSelectedUser(event: Event) {
     const userId = parseInt((event.target as HTMLSelectElement)?.value);
-    this.todosById$ = this.todoService.getTodosByUserId(userId).pipe(
-      map((todos) => todos.filter((todo) => !this.completedFilter || todo.completed))
-    );
-    this.postsByUserId$ = this.postService.getPostsByUserId(userId);
+    this.selectedUserId$.next(userId);
   }
-
 }
