98b2359dbe2833c83a88cf7d05b064d48c761ee9
vblaskovics <viktor.blaskovics@gmail.com>
2022-11-09 14:55:28 +0100

    Clear Interval on Bret's page

--- 01_intro/src/app/pages/landing-todobyname/landing-todobyname.component.ts
+++ 01_intro/src/app/pages/landing-todobyname/landing-todobyname.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Todo } from 'src/app/todos/todo';
 import { TodoService } from 'src/app/todos/todo.service';
 import { Observable } from 'rxjs';
@@ -8,20 +8,30 @@ import { Observable } from 'rxjs';
   templateUrl: './landing-todobyname.component.html',
   styleUrls: ['./landing-todobyname.component.css']
 })
-export class LandingTodobynameComponent implements OnInit {
+export class LandingTodobynameComponent implements OnInit, OnDestroy {
   todos$: Observable<Todo[]> = new Observable<Todo[]>();
   todoArr: Todo[] = [];
+  intervalCall:any;
 
   constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
-    setInterval(()=> {
+    // this.intervalCall = setInterval(()=> {
+    //   console.log("Get Bret's todos");
+    //   this.todos$ = this.todoService.getTodosByUsername('Bret');
+    //   this.todos$.subscribe((todos) => {
+    //     this.todoArr = todos
+    //   });
+    // }, 3000);
+    this.intervalCall = setInterval(()=> {
       console.log("Get Bret's todos");
       this.todos$ = this.todoService.getTodosByUsername('Bret');
-      this.todos$.subscribe((todos) => {
-        this.todoArr = todos
-      });
     }, 3000)
+  }
+
+  ngOnDestroy(): void {
+      console.log('destroy');
+      clearInterval(this.intervalCall);
   }
 
 }
