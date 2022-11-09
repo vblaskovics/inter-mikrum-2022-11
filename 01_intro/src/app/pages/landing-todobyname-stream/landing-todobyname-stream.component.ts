3046352021789f2adc20dba7110daf885484032a
vblaskovics <viktor.blaskovics@gmail.com>
2022-11-09 15:38:17 +0100

    Add todo stream

--- 01_intro/src/app/pages/landing-todobyname-stream/landing-todobyname-stream.component.ts
+++ 01_intro/src/app/pages/landing-todobyname-stream/landing-todobyname-stream.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from 'src/app/todos/todo.service';
-import { timer } from 'rxjs';
+import { combineLatest, map, Observable, switchMap, tap, timer } from 'rxjs';
+import { Todo } from 'src/app/todos/todo';
 
 @Component({
   selector: 'app-landing-todobyname-stream',
   templateUrl: './landing-todobyname-stream.component.html',
-  styleUrls: ['./landing-todobyname-stream.component.css']
+  styleUrls: ['./landing-todobyname-stream.component.css'],
 })
 export class LandingTodobynameStreamComponent implements OnInit {
-
-  constructor(private todoService:TodoService) { }
+  todos$:Observable<Todo[]> = new Observable<Todo[]>;
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    timer(0, 2000).subscribe((e) => {
-      console.log(e);
-    })
-  }
+    this.todos$ = timer(0, 2000).pipe(
+      switchMap(() => {
+        return this.todoService.getTodosByUsername('Bret')
+      })
+    );
 
+    // this.todos$ = combineLatest([
+    //   timer(0, 2000),
+    //   this.todoService.getTodosByUsername('Bret'),
+    // ]).pipe(map((res) => res[1]));
+
+  }
 }
