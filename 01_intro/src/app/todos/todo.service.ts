b69891003cb0cee587fba959635eb15d65052e15
vblaskovics <viktor.blaskovics@gmail.com>
2022-11-09 13:58:49 +0100

    Add getTodosByUsername

--- 01_intro/src/app/todos/todo.service.ts
+++ 01_intro/src/app/todos/todo.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { HttpClient } from '@angular/common/http'
 import { Todo } from './todo';
+import { UserService } from '../users/user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { Todo } from './todo';
 export class TodoService {
   private API:string = 'https://jsonplaceholder.typicode.com/todos'
   
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private userService:UserService) {}
 
   getTodos(): Observable<Todo[]> {
     return this.httpClient.get<Todo[]>(this.API);
@@ -17,6 +18,12 @@ export class TodoService {
 
   getTodosByUserId(userId: number): Observable<Todo[]> {
     return this.httpClient.get<Todo[]>(`${this.API}?userId=${userId}`);
+  }
+
+  getTodosByUsername(username:string) {
+    return this.userService.getUserByUsername(username).pipe(
+      switchMap((user)=> this.getTodosByUserId(user.id)),
+    );
   }
 
 }
