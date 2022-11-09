7c01c0ed422ab2d36285991bc66c4763cc5b0f09
vblaskovics <viktor.blaskovics@gmail.com>
2022-11-09 09:53:53 +0100

    Add posts with username with subscribe

--- 01_intro/src/app/app.component.ts
+++ 01_intro/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { PostService } from './posts/post.service';
+import { UserService } from './users/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '01_intro';
+
+  constructor(
+    private userService: UserService,
+    private postService: PostService
+  ) {}
+
+  ngOnInit() {
+    this.getPostsWithUsernameSubscribe();
+  }
+
+  getPostsWithUsernameSubscribe(): void {
+    this.userService.getUsers().subscribe((users) => {
+      this.postService.getPosts().subscribe((posts) => {
+        const postsWithUsername = posts.map((post) => {
+          const user = users.find((user) => user.id === post.userId);
+          if (!user) return post;
+          return { ...post, username: user.username };
+        });
+        console.log('Posts with username', postsWithUsername);
+      });
+    });
+  }
 }
