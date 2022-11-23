29f67b34f5ebaf05b77bc7edb43e7e672924d5a7
vblaskovics <viktor.blaskovics@gmail.com>
2022-11-23 10:36:19 +0100

    Add header component

--- 03_structure/src/app/app.component.spec.ts
+++ 03_structure/src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, AppModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -26,17 +23,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('03_structure');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('03_structure app is running!');
-  });
-
   it('should have a header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('[data-test="header"]')).withContext("header not found").toBeTruthy();
-  })
+    expect(compiled.querySelector('[data-test="header"]'))
+      .withContext('header not found')
+      .toBeTruthy();
+  });
 });
